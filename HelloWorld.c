27abd196138a81277efb36659f974c0c77a08b8b
monbernard14852 <monbernard14852@gmail.com>
2021-01-12 22:53:33 +0700

    first commit

---
+++ HelloWorld.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+// ส่วนหัวสำหรับดึง Libraty(1)
+
+// ส่วน function หลัก(2)
+int main()
+{
+    printf("Kritidaj Jangmongkulkarn\n");
+    printf("Mon\n");
+    printf("19\n");
+    printf("game\n");
+    printf("Play game\n");
+    printf("I need grade A\n");
+    /*
+        ส่วนคำสั่งการทำงาน (3)
+    */
+    // วิธีการจัดชุดคำสั่งให้สวย ให้ Shitf + alt + f
+    // การเปิด Terminal ให้กด ctrl + j
+}
